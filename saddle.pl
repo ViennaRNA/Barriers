888f0c084a0eaef633198191273f394a2d2083e7
Ivo Hofacker <ivo@tbi.univie.ac.at>
2001-04-05 09:52:53 +0000

    add -d3 option

--- saddle.pl
+++ saddle.pl
@@ -1,6 +1,6 @@
-#!/usr/local/bin/perl -w -I/scr/linse/ivo/WORK/RNA/Barriers
+#!/usr/bin/perl -w
 # -*-Perl-*-
-# Last changed Time-stamp: <2000-03-21 11:48:28 ivo>
+# Last changed Time-stamp: <2001-01-25 10:07:25 ivo>
 
 use RNA;
 use Getopt::Long;
@@ -9,12 +9,12 @@ use strict;
  Getopt::Long::config('no_ignore_case');
 
 use vars '$max', '$max_en', '$opt_4', '$opt_d', '$opt_d2', '$ParamFile',
-    '$opt_v';
+    '$opt_v','$opt_d3' ;
 $max = 20;
 $max_en = 99999.;
 
 &usage() unless GetOptions("T=f" => \$RNA::temperature,
-                           "4", "d", "d2",
+                           "4", "d", "d2", "d3",
                            "logML" => \$RNA::logML,
                            "P=s" => \$ParamFile,
                            "max=i"=> \$max,
@@ -24,13 +24,17 @@ $max_en = 99999.;
 $RNA::tetra_loop = 0 if ($opt_4);
 $RNA::dangles = 0 if ($opt_d);
 $RNA::dangles = 2 if ($opt_d2);
+$RNA::dangles = 3 if ($opt_d3);
 
- RNA::read_parameter_file($ParamFile) if ($ParamFile);
+ RNA::read_parameter_file($ParamFile) if (defined($ParamFile));
 
 # importing barrier.pl will call RNA::update_fold_params()
 # this should really be  use RNA::barrier;
+use lib '/home/linse/ivo/RNA/barriers';
 use barrier;
-
+ 
+ RNA::read_parameter_file($ParamFile) if (defined($ParamFile));
+RNA::update_fold_params();
 print "Input sequence and two structures\n" .
     "....,....1....,....2....,....3....,....4" .
     "....,....5....,....6....,....7....,....8\n"
