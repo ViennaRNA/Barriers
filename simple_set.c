82dd8c8402ed88c9952b9e718321cc49888f580e
Ivo Hofacker <ivo@tbi.univie.ac.at>
2001-07-09 08:55:29 +0000

    implment set via sorted array

---
+++ simple_set.c
@@ -0,0 +1,82 @@
+/* Last changed Time-stamp: <2001-07-05 16:51:16 ivo> */
+
+/* implement set as sorted array */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "utils.h"
+#include "hash_util.h"
+#include "simple_set.h"
+
+
+static int comp_basinT(const void *a, const void *b) {
+  int A, B;
+  A = ((basinT *)a)->basin; B = ((basinT *)b)->basin;
+  return (A - B);
+}
+
+Set *new_set(int elems) {
+  Set *set;
+  set = space(sizeof(Set));
+  set->data = space(sizeof(basinT)*(elems+1));
+  set->max_elem=elems;
+  return set;
+}
+
+int set_find(Set *set, basinT *data) {
+  int c=1, i=0, i2, i1=0;
+  i2 = set->num_elem-1;
+  while (i1<=i2) {
+    i = (i1+i2)/2;
+    c = comp_basinT(set->data+i, data);
+    if (c>0) i2 = i-1;
+    if (c<0) i1 = i+1;
+    if (c==0) break;
+  }
+  if (c==0) return i;
+  else return -i1-1;
+}
+
+int set_add(Set *set, basinT *data) {
+  int pos;
+  if ((pos=set_find(set, data))>=0) return 0;
+  /* else insert before -pos-1 */
+  pos = -pos-1;
+  set->num_elem++;
+  if (set->max_elem <= set->num_elem) {
+    set->max_elem *= 2;
+    set->data = xrealloc(set->data, sizeof(basinT)*(set->max_elem+1));
+  }
+  memmove(set->data+pos+1, set->data+pos, (set->num_elem-pos)*sizeof(basinT));
+  set->data[pos] = *data;
+  return 1;
+}
+
+void set_kill(Set *set) {
+  free(set->data);
+  free(set);
+}
+
+int set_merge(Set *s1, const Set *s2) {
+  int i, i1, i2, num;
+  basinT *ndata;
+  num = s1->num_elem + s2->num_elem + 1;
+  if (num<s1->max_elem) num=s1->max_elem;
+  ndata = (basinT *) space((num+1)*sizeof(basinT));
+  for (i=i1=i2=0; i1<s1->num_elem || i2<s2->num_elem; i++) {
+    int c;
+    if (i1==s1->num_elem) c= +1;
+    else if (i2==s2->num_elem) c= -1;
+    else
+      c = comp_basinT(s1->data+i1, s2->data+i2);
+    ndata[i] = (c<0) ?  s1->data[i1] : s2->data[i2];
+    if (c<=0) i1++;
+    if (c>=0) i2++;
+  }
+  free(s1->data); s1->data=ndata;
+  s1->max_elem=num; s1->num_elem=i;
+  return i;
+}
+
+  
+/* End of file */
---
+++ simple_set.h
@@ -0,0 +1,25 @@
+/* simple_set.h */
+/* Last changed Time-stamp: <2001-07-04 20:31:06 ivo> */
+#ifndef SIMPLE_SET_H
+#define SIMPLE_SET_H
+
+typedef struct {
+  int basin;
+  hash_entry *hp;
+} basinT;
+
+typedef struct set {
+  int num_elem;
+  int max_elem;
+  size_t elem_size;
+  basinT *data;
+} Set;
+
+extern Set *new_set(int elems);
+extern int set_add(Set *set, basinT *data);
+extern void set_kill(Set *set);
+extern int set_merge(Set *s1, const Set *s2);
+extern int set_find(Set *set, basinT *data);
+
+#endif
+/* End of file */
