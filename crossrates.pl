a7c3b8d6b796401d082c3a9b5d42400d8cc59c41
Michael T. Wolfinger <michael@wolfinger.eu>
2017-10-20 16:58:49 +0200

    crossrates functional; computes correct cross-terms in rate matrix

--- crossrates.pl
+++ crossrates.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # -*-CPerl-*-
-# Last changed Time-stamp: <2017-10-16 17:44:00 mtw>
+# Last changed Time-stamp: <2017-10-20 15:48:54 mtw>
 
 use Getopt::Long;
 use Data::Dumper;
@@ -9,20 +9,31 @@ use File::Basename;
 use strict;
 use warnings;
 
-my ($sequence,$unbound,$bound,$fhu,$fhb,$fhra,$fhrb);
-my ($basename,$bardir,$suffix,$ratesr,$dim);
+my ($sequence,$fnunbound,$fnbound,$fhu,$fhb,$fhra,$fhrb);
+my ($ratesr,$dim,$fnrbr,$fhrbr);
 my $bar=undef;
 my $rates="rates.out";
 my $binrates="rates.bin";
 my %lmins_b = (); # indices of ligand-bound lmins
 my %lmins_u = (); # indices of unbound lmins
 my @bar_o   = (); # AoH holding the original bar file
+my $T=37.;
+my $K0=-273.15;
+my $DuplexInit37=410;
+my $DuplexInitdH=360;
+my $DuplexInit = 0.;
+my $Conc = 1.;
+my $BindingBonus = 0.;
+
 
 Getopt::Long::config('no_ignore_case');
 pod2usage(-verbose => 1) unless GetOptions(
 					   "b|bar=s"    => \$bar,
 					   "rates=s"    => \$rates,
 					   "binrates=s" => \$binrates,
+					   "T|temp=f"   => \$T,
+					   "C|conc=f"   => \$Conc,
+					   "E|energy=f" => \$BindingBonus,
 					   "man"        => sub{pod2usage(-verbose => 2)},
                                            "h|help"     => sub{pod2usage(1)}
 					  );
@@ -39,19 +50,33 @@ unless (-f $bar){
   pod2usage(-verbose => 0);
 }
 if($bar =~ m/\.bar$/){
-  ($basename,$bardir,$suffix) = fileparse($bar,qr/\.bar/);
-  $bound = $bardir.$basename.".bound".$suffix;
-  $unbound = $bardir.$basename.".unbound".$suffix;
+  my ($basename,$dir,$suffix) = fileparse($bar,qr/\.bar/);
+  $fnbound = $dir.$basename.".bound".$suffix;
+  $fnunbound = $dir.$basename.".unbound".$suffix;
 }
 else{
-  $bound = $bar.".bound";
-  $unbound = $bar.".unbound";
+  $fnbound = $bar.".bound";
+  $fnunbound = $bar.".unbound";
 }
-open $fhu, ">", $unbound
-  or die "Cannot open filehandle for unbound: $!\n";
-open $fhb, ">", $bound
-  or die "Cannot open filehandle for bound: $!\n";
+if($binrates =~ m/\.bin$/){
+  my ($basename,$dir,$suffix) = fileparse($binrates,qr/\.bin/);
+  $fnrbr = $dir.$basename.".r".$suffix;
+}
+else{
+  $fnrbr = $binrates.".r";
+}
+
+scale_duplexInitiationEnergy($T);
+print "DuplexInit: $DuplexInit\n";
+my $kT = 0.00198717*($K0+$T);
+my $Beta = 1/$kT;
+
+open $fhu, ">", $fnunbound
+  or die "Cannot open filehandle for unbound bar file: $!\n";
+open $fhb, ">", $fnbound
+  or die "Cannot open filehandle for bound bar file: $!\n";
 $dim = parse_barfile($bar);
+print "dim is $dim\n";
 consistify(\%lmins_u,$fhu);
 consistify(\%lmins_b,$fhb);
 close($fhu);
@@ -60,12 +85,18 @@ close($fhb);
 #print Dumper(\%lmins_b);
 open $fhra, "<", $rates
   or die "Cannot open filehandle for ASCII rates file: $!\n";
+close($fhra);
+
 open $fhrb, "<:raw", $binrates
-  or die "Cannot open filehandle for binary rates file: $!\n";
+  or die "Cannot open filehandle for reading binary rates file: $!\n";
+open $fhrbr, ">:raw", $fnrbr
+  or die "Cannot open filehandle for writing binary rates file: $!\n";
 $rates = parse_binrates($fhrb);
 $ratesr = reorder_matrix($rates,\%lmins_u,\%lmins_b,$dim);
-close($fhra);
+#dump_matrix($ratesr,$dim);
+write_binrates($ratesr,$fhrbr,$dim);
 close($fhrb);
+close($fhrbr);
 
 sub consistify {
   my ($B,$fh) = @_;
@@ -136,13 +167,17 @@ sub parse_binrates {
   @m = unpack("id*", <$fh>);
   $d = shift @m;
   print "$d\n";
-  for my $row (0..$d-1){
-    for my $col (0..$d-1){
-   #   printf (STDOUT "%10.4g ", $m[$d*$row+$col]);
-    }
-#    printf (STDOUT "\n");
-  }
   return \@m;
+}
+
+sub write_binrates {
+  my ($r,$fh,$d) = @_;
+  print $fh pack("i", $d);
+  for (my $i=0;$i<$d;$i++){
+    for (my $j=0;$j<$d;$j++){
+      print $fh pack("d", $$r[$d*$i+$j]);
+    }
+  }
 }
 
 sub reorder_matrix {
@@ -159,23 +194,29 @@ sub reorder_matrix {
       $n[$dim*$i+$j]=0.;
     }
   }
+  
+  print "LU:\n";
+  print Dumper($lu);
+  print "LB:\n";
+  print Dumper($lb);
 
   # merge lookup lmin hashes
   foreach my $key (keys %$lu){ # unbound states
     if($$lu{$key}>$maxi){$maxi =  $$lu{$key}}
     $lm{$key} = $$lu{$key};
   }
+  print "maxi=$maxi\n";
   foreach my $key (keys %$lb){ # bound states
-    die "inconsistency if lmin hash: lmin $key already assigned\n"
+    die "inconsistency in lmin hash: lmin $key already assigned\n"
       if (exists $lm{$key});
     $lm{$key} = $$lb{$key}+$maxi;
   }
+
   #print Dumper(\%lm);
   %lmr = reverse %lm;
   #print Dumper(\%lmr);
   for($i=1;$i<=$dim;$i++){
     $oi=$lmr{$i};
-    #print "oi=$oi\n";
     $oi--;
     for($j=1;$j<=$dim;$j++){
       $oj=$lmr{$j}-1;
@@ -183,7 +224,32 @@ sub reorder_matrix {
     }
 #    print "\n";
   }
-  dump_matrix(\@n,$dim);
+
+  my $a = adjust_crossterms(\@n,$maxi,$dim);
+#  dump_matrix($a,$dim);
+
+  
+  return $a;
+}
+
+sub  adjust_crossterms{
+  my ($mx,$max,$dim) = @_;
+  # dump_matrix($mx,$dim);
+  for (my $i=0;$i<$max;$i++){ # ON rate
+    for(my $j=$max;$j<$dim;$j++){
+      # $$mx[$dim*$i+$j] *= $Conc*$DuplexInit;
+      $$mx[$dim*$j+$i] *= $Conc*$DuplexInit;
+    }
+  }
+
+   for (my $i=$max;$i<$dim;$i++){ # OFF rate
+    for(my $j=0;$j<$max;$j++){
+      # $$mx[$dim*$i+$j] *= $DuplexInit * exp(-$Beta*$BindingBonus);
+      $$mx[$dim*$j+$i] *= $DuplexInit * exp(-$Beta*$BindingBonus);
+    }
+  }
+  # dump_matrix($mx,$dim);
+  return $mx;
 }
 
 sub dump_matrix {
@@ -194,6 +260,14 @@ sub dump_matrix {
     }
     print STDOUT "\n";
   }
+}
+
+
+sub scale_duplexInitiationEnergy {
+  my $T = shift;
+  my $tempf  = (($T + $K0) / (37 + $K0));
+  $DuplexInit = $DuplexInitdH - ($DuplexInitdH - $DuplexInit37) * $tempf;
+  $DuplexInit /= 100.;
 }
 
 __END__
@@ -211,9 +285,9 @@ crossrates.pl [-b|--bar I<FILE>] [options]
 This script is a post-processing utility for "barriers -M ligand"
 calls.
 
-It adjusts splits ligand bound (aka 'starred') and unbound (regular)
-states into separate bar files and adjusts for ligand concentration in
-rates matrices produced by barriers.
+It deconvolutes ligand bound (aka 'starred') and unbound (regular)
+states into separate bar files, reorganizes barrier rates matrices and
+adjusts them for different ligand binding energies, concentrations.
 
 =head1 OPTIONS
 
@@ -222,6 +296,10 @@ rates matrices produced by barriers.
 =item B<-b|--bar>
 
 RNA energy landscape in Barriers format (.bar file)
+
+=item B<-T|--temp>
+
+Simulation temperature in Celsius (default: 37.0)
 
 =item B<--binrates>
 
